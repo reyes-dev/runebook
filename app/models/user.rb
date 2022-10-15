27d5b2780d4cfefb04d4de3192e5be000ea7cbe1
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-10-15 00:35:43 -0700

    Set up FriendRequest model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :received_friend_requests, foreign_key: "receiver_id", class_name: "FriendRequest"
+  has_many :sent_friend_requests, foreign_key: "sender_id", class_name: "FriendRequest"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
