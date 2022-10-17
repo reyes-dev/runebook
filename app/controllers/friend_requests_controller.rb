b72a94ee19f37c1c28c48d16ffb9ae1793192558
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-10-16 20:33:25 -0700

    Prevent users from sending friend requests to themselves

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,7 +1,9 @@
 class FriendRequestsController < ApplicationController
   def create
-    @friend_request = FriendRequest.create friend_request_params
-    @reverse_friend_request = FriendRequest.create(:sender_id => friend_request_params[:receiver_id], :receiver_id => friend_request_params[:sender_id])
+    unless params[:friend_request][:receiver_id].to_i == current_user.id
+      @friend_request = FriendRequest.create friend_request_params
+      @reverse_friend_request = FriendRequest.create(:sender_id => friend_request_params[:receiver_id], :receiver_id => friend_request_params[:sender_id])
+    end
 
     redirect_to users_path
   end
