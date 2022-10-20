4fb68e8b8f4b89fa2b5623f970fb0ccb944f62e3
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-10-20 01:06:44 -0700

    Add Accept Friend Request button

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,17 @@
 class FriendshipsController < ApplicationController
     def create
+        @friendship = Friendship.create friendship_params
+        @reverse_friendship = Friendship.create(:user_id => friendship_params[:friend_id], :friend_id => friendship_params[:user_id])
+
+        redirect_to user_friend_requests_path(current_user.id)
     end
 
     def destroy
+    end
+
+    private
+
+    def friendship_params
+        params.require(:friendship).permit(:user_id, :friend_id)
     end
 end
