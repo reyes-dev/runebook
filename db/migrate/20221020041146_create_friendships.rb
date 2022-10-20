23389b4d2e6c6b3015fa3eb8775a69cb7110f2d8
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-10-19 21:19:56 -0700

    Add Friendship model

---
+++ db/migrate/20221020041146_create_friendships.rb
@@ -0,0 +1,10 @@
+class CreateFriendships < ActiveRecord::Migration[7.0]
+  def change
+    create_table :friendships do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :friend_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_050721) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_041146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_050721) do
     t.integer "sender_id"
     t.integer "receiver_id"
     t.boolean "received"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -42,5 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_050721) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friendships", "users"
   add_foreign_key "posts", "users"
 end
