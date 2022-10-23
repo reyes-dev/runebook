0b2ad30f00c41213febbccffde9f08ebba8bba53
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-10-22 17:27:56 -0700

    Set up Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_041146) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_23_000742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "friend_requests", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -50,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_041146) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "friendships", "users"
   add_foreign_key "posts", "users"
 end
