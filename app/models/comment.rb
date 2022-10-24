f0135d9bc65b6b948be1754dbd21c902d8560357
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-10-23 22:55:57 -0700

    Add Like model associations on Post/Comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+  has_many :likes, as: :likeable
   belongs_to :post
   belongs_to :user
   validates :body, :user_id, :post_id, presence: true
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  has_many :likes, as: :likeable
   has_many :comments
+  belongs_to :user
   validates :body, presence: true
   validates_length_of :body, :maximum => 280
   
