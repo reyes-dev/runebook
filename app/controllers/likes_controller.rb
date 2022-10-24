1bc2c4bb100ce9f81e14a1dd2f46be2252ee9571
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-10-24 02:09:26 -0700

    Add Like functionality to posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,20 @@
 class LikesController < ApplicationController
     def create
+        @like = Like.new like_params
+        @like.user = current_user
+        if @like.save
+            redirect_to posts_path
+        else
+            redirect_to posts_path, status: :unprocessable_entity
+        end
     end
 
     def destroy
+    end
+
+    private
+
+    def like_params
+        params.require(:like).permit(:user_id, :likeable_id, :likeable_type)
     end
 end
