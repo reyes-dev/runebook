bb09b36fdf7b5220387e699e67959f8578bfddfd
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-10-26 02:59:15 -0700

    Set up Profile editing

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,21 @@
 class ProfilesController < ApplicationController
     def edit
+        @profile = Profile.find(params[:id])
     end
 
     def update
+        @profile = Profile.find(params[:id])
+        
+        if @profile.update profile_params
+            redirect_to edit_profile_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def profile_params
+        params.require(:profile).permit(:information, :image)
     end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,4 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  has_attached_file :image,
-  :storage => :cloudinary
   has_one_attached :image
 end
