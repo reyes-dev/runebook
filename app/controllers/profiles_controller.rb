bbec086426b8caf54ae7f5d17a39e52e81945d0e
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-10-26 02:20:31 -0700

    Set up Profiles controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,7 @@
+class ProfilesController < ApplicationController
+    def edit
+    end
+
+    def update
+    end
+end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,5 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_attached_file :image,
   :storage => :cloudinary
+  has_one_attached :image
 end
