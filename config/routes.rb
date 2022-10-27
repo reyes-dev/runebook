16a30f3e6255c97730c75c99adb960b0bd9fc34b
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-10-27 00:16:42 -0700

    Remove conflicting route

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
-
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
   
   devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
