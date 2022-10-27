5cdba6ce031588b8d35af877785d5bc1364e9db3
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-10-27 00:04:23 -0700

    Add route to cancel sign up with Facebook

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
+
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
   
   devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
