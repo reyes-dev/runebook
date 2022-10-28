710d2aba1b6f2f278bdd123037c1d2a9058dcc8d
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-10-27 21:38:23 -0700

    Add welcome_email method to UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+    default from: 'runebooktop@gmail.com'
+
+    def welcome_email
+        @user = params[:user]
+        @url  = 'http://localhost:3000/'
+        mail(to: @user.email, subject: 'Welcome to Runebook')
+    end
 end
