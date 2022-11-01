a36cce7dfe5ce2746da714e5d27b794b633d7b13
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-11-01 02:43:02 -0700

    Update seeds file to generate 100 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,22 @@
 require 'faker'
+require 'open-uri'
 
-100.times do
-    User.create!(email: Faker::Internet.email, username: Faker::Name.name, password: Faker::Internet.password(min_length: 10, max_length: 16))
+50.times do |i|
+    user = User.create!(email: Faker::Internet.email, username: Faker::Name.male_first_name + Faker::Name.last_name, password: Faker::Internet.password(min_length: 10, max_length: 16))
+
+    url = URI.parse("https://xsgames.co/randomusers/assets/avatars/male/#{i}.jpg")
+    filename = File.basename(url.path)
+    file = URI.open(url)
+    user.profile.image.attach(io: file, filename: filename)
 end
+
+50.times do |i|
+    user = User.create!(email: Faker::Internet.email, username: Faker::Name.female_first_name + Faker::Name.last_name, password: Faker::Internet.password(min_length: 10, max_length: 16))
+
+    url = URI.parse("https://xsgames.co/randomusers/assets/avatars/female/#{i}.jpg")
+    filename = File.basename(url.path)
+    file = URI.open(url)
+    user.profile.image.attach(io: file, filename: filename)
+end
+
+5.times { |i| User.create(email: "User_#{i}@runebook.moe", username: Faker::Name.name, password: "password") }
