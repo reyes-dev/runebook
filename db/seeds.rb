24b4c823a616995055ae273ce68f19d9f615ada7
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-11-13 06:15:25 -0800

    Update seed file to simulate activity with profile info, posts, comments and pre-existing friendships/friend requests

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@ require 'open-uri'
     filename = File.basename(url.path)
     file = URI.open(url)
     user.profile.image.attach(io: file, filename: filename)
+    user.profile.update(information: Faker::Lorem.sentence(word_count: 10))
 end
 
 50.times do |i|
@@ -17,6 +18,33 @@ end
     filename = File.basename(url.path)
     file = URI.open(url)
     user.profile.image.attach(io: file, filename: filename)
+    user.profile.update(information: Faker::Lorem.sentence(word_count: 10))
 end
 
-5.times { |i| User.create(email: "User_#{i}@runebook.moe", username: Faker::Name.name, password: "password") }
+(1..5).each do |i| 
+    User.create(email: "User_#{i}@example.com", username: Faker::Name.name, password: "password")
+end
+
+
+
+5.times do |i|
+    5.times do |x|
+        user_id = "10#{i}".to_i
+        Friendship.create(user_id: user_id, friend_id: x)
+    end
+end
+
+3.times do |i|
+    3.times do |x|
+        user_id = "10#{i}".to_i
+        FriendRequest.create(sender_id: x + 10, receiver_id: user_id, received: true)
+        FriendRequest.create(sender_id: user_id, receiver_id: x + 10, received: false)
+    end
+end
+
+(1..5).each do |i|
+    Post.create(body: Faker::Lorem.sentence(word_count: 30), user_id: i)
+    rand(1..4).times do
+        Comment.create(body: Faker::Lorem.sentence(word_count: 10), post_id: Post.last.id, user_id: rand(1..5))
+    end
+end
