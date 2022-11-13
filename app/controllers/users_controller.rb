95b29bfb8a91e8f01187f6821b34f7654decb43e
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-11-13 02:49:16 -0800

    Create a variable to hold the current user and grab all users except current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.includes(:profile).includes(:profile => [image_attachment: :blob]).includes(:received_friend_requests).order("RANDOM()")
+    @users = User.all_except(current_user).includes(:profile).includes(:profile => [image_attachment: :blob]).includes(:received_friend_requests).order("RANDOM()")
     @current_user = current_user
   end
 
   def show
-    @user = User.find(params[:id])
-    @posts = @user.posts
+    @current_user = User.find(params[:id])
+    @posts = @current_user.posts
   end
 end
