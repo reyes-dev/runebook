7cdeaa1dd872a4285c805a3df5be939ff4a090b4
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-11-20 00:36:28 -0800

    Stop associating Like model with Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ApplicationRecord
-  has_many :likes, as: :likeable, dependent: :destroy
   belongs_to :post
   belongs_to :user
   validates :body, :user_id, :post_id, presence: true
