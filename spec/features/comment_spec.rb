5237af8c1616ebc49808a49c2bbdbb621b33f188
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-11-21 23:52:56 -0800

    Add comment spec for posting a comment with valid inputs

---
+++ spec/features/comment_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Comment', type: :feature, :js => true do
+    let!(:user1) { FactoryBot.create(:user, id: 1) }
+    let!(:post1) { FactoryBot.create(:post, user: user1) }
+
+    scenario 'valid inputs' do
+        login_as(user1)
+        visit posts_path
+        click_button 'Comment'
+        fill_in 'comment_body', with: 'comment text'
+        click_on 'Post Comment'
+        visit posts_path
+        # sleep 3.seconds
+        expect(page).to have_content('comment text')
+    end
+end
