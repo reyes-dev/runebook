5df3323d3febb9fff0d37f0676012d02d0d67d29
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-11-23 01:25:41 -0800

    Add comment spec for posting a comment with invalid input

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe 'Comment', type: :feature, :js => true do
         # sleep 3.seconds
         expect(page).to have_content('comment text')
     end
+
+    scenario 'invalid inputs' do
+        login_as(user1)
+        visit posts_path
+        click_button 'Comment'
+        fill_in 'comment_body', with: ' '
+        click_on 'Post Comment'
+        sleep 3.seconds
+        expect(page).to have_content('Invalid comment.')
+    end
 end
