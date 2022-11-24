85c5bdb40eba0fa677be5abb5cd58b229b2418e2
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-11-24 00:25:32 -0800

    Add post spec for creating a post with invalid input

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -9,7 +9,16 @@ RSpec.describe 'Post', type: :feature, :js => true do
         fill_in 'post[body]', with: 'post text'
         click_button 'Submit Post'
         visit posts_path
-        sleep 3.seconds
+        # sleep 3.seconds
         expect(page).to have_content('post text')
+    end
+
+    scenario 'invalid inputs' do
+        login_as(user1)
+        visit posts_path
+        fill_in 'post[body]', with: ' '
+        click_button 'Submit Post'
+        # sleep 3.seconds
+        expect(page).to have_content('Invalid post')
     end
 end
