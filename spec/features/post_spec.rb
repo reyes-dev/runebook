bf0de7b7c2f942b27025ea0de15b0b52b82c0af7
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-11-24 00:20:55 -0800

    Add post spec for creating a post with valid input

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Post', type: :feature, :js => true do
+    let!(:user1) { FactoryBot.create(:user, id: 1) }
+
+    scenario 'valid inputs' do
+        login_as(user1)
+        visit posts_path
+        fill_in 'post[body]', with: 'post text'
+        click_button 'Submit Post'
+        visit posts_path
+        sleep 3.seconds
+        expect(page).to have_content('post text')
+    end
+end
