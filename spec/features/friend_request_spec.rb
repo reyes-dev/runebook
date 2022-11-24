f020a81442042145e267d4d31356079891f0b575
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-11-24 03:58:57 -0800

    Add a test to friend_request_spec.rb to reject friend request on receiving users end

--- spec/features/friend_request_spec.rb
+++ spec/features/friend_request_spec.rb
@@ -17,9 +17,19 @@ RSpec.describe 'Friend Request', type: :feature do
         login_as(user1)
         visit users_path
         click_on 'Send Friend Request'
-        visit users_path
         login_as(user2)
         visit user_friend_requests_path(user_id: user2.id)
         expect(page).to have_content(user1.username)
+    end
+
+    scenario 'send a friend request and reject it on the receiving users end' do
+        login_as(user1)
+        visit users_path
+        click_on 'Send Friend Request'
+        login_as(user2)
+        visit user_friend_requests_path(user_id: user2.id)
+        expect(page).to have_content(user1.username)
+        click_on 'Reject'
+        expect(page).not_to have_content(user1.username)
     end
 end
