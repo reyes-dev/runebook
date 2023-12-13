202307c94048c6ecf5d645aa7692f4b8fbcfbf08
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-12-13 13:47:17 -0800

    Break users page into partials, fix sidebar bug related to users controller, clean up spacing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all_except(current_user).includes(:profile).includes(:profile => [image_attachment: :blob]).includes(:received_friend_requests).order("RANDOM()")
+    @users = User.all_except(current_user).includes(:profile).includes(profile: [image_attachment: :blob]).includes(:received_friend_requests).order('RANDOM()')
     @current_user = current_user
   end
 
   def show
-    @current_user = User.find(params[:id])
-    @posts = @current_user.posts
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 end
